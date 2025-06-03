61c5776113a3fea6f5aa315a80b9d41aa628d976
zhouyongyou <128128010+zhouyongyou@users.noreply.github.com>
2025-06-04 05:12:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("blocksLeft").textContent = "Coming Soon...";
+document.getElementById("timeLeft").textContent = "Coming Soon...";
+document.getElementById("poolAmount").textContent = "Coming Soon...";
+document.getElementById("lastWinner").textContent = "Coming Soon...";
