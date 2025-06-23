54f91c9e301a9bc85fd5c4d58327c88f92ed3e83
ZhouYongyou <128128010+zhouyongyou@users.noreply.github.com>
2025-06-23 23:33:11 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,13 @@ import { WagmiProvider, createConfig, http } from 'wagmi';
 import { bsc } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// 建立 Wagmi 設定
 const config = createConfig({
-  chains: [bsc], // 設定支援的鏈為幣安智能鏈
+  chains: [bsc],
   transports: {
-    [bsc.id]: http(), // 使用預設的公共 RPC
+    [bsc.id]: http(),
   },
 });
 
-// 建立 React Query 用戶端
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
